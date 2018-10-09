55859ec492c33338626f9382118a1b3a1eedcb1c
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-10-09 17:54:02 +0300

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,11 +10,38 @@
 
     <section class="section">
         <div class="container">
-            <h2 class="section__title">Наши услуги</h2>
+            <h2 class="section__title">То, сто мы делаем</h2>
+            <p class="section__subtitle">Основные услуги нашей компании</p>
         </div>
     </section>
-    <section class="section"></section>
-    <section class="section"></section>
-    <section class="section"></section>
-    
+    <section class="section">
+        <div class="container">
+            <h2 class="section__title">Кто мы и как работаем</h2>
+            <p class="section__subtitle">Коротко о нас и этапах сотрудничества</p>
+        </div>
+    </section>
+    <section class="section">
+        <div class="container">
+            <h2 class="section__title">Почему мы?</h2>
+            <p class="section__subtitle">Наши основные преимущества</p>
+        </div>
+    </section>
+    <section class="section">
+        <div class="container">
+            <h2 class="section__title">Наши работы</h2>
+            <p class="section__subtitle">Список некоторых проектов выполненных нашей компанией</p>
+        </div>
+    </section>
+    <section class="section">
+        <div class="container">
+            <h2 class="section__title">Нас рекомендуют</h2>
+            <p class="section__subtitle">Несколько отзывов от наших клиентов</p>
+        </div>
+    </section>
+    <section class="section">
+        <div class="container">
+            <h2 class="section__title">Наши контакты</h2>
+            <p class="section__subtitle">Получите бесплатную консультацию специалиста</p>
+        </div>
+    </section>
 @endsection
